1a71264ce4dc51a398f23a2a41487180b512dabd
Yu Luo <yu.8.luo@gsk.com>
2023-10-09 13:06:18 -0400

    mpc_optimizer.py: added log_sample method in Bioreactor

--- src/mpc/mpc_optimizer.py
+++ src/mpc/mpc_optimizer.py
@@ -5,6 +5,7 @@
 """
 
 import numpy as np
+import pandas as pd
 
 class Bioreactor:
     """Bioreactor object class for simulation and also processing real off-line data
@@ -13,11 +14,37 @@ class Bioreactor:
         Modified: 2023-10-06
     """
 
-    def __init__(self):
-        self.vessel = None # Vessel name for processing multiple bioreactors
-        self.model = None # Model (if provided) for simulating a process
-        self.data = None # Data for storing simulation results or real data (if provided)
-        self.state = None # Current state of the process
+    def __init__(
+            self,
+            vessel = None,
+            process_model = None,
+            data = pd.DataFrame,
+            state = None,
+            duration = 12,
+            curr_time = 0):
+        self.vessel = vessel # Vessel name for processing multiple bioreactors
+        self.process_model = process_model # Model (if provided) for simulating a process
+        self.data = data # Data for storing simulation results or real data (if provided)
+        self.state = state # Current state of the process
+        self.duration = duration # Full culture duration
+        self.curr_time = curr_time # Current culture day
+
+
+    def log_sample(
+            self,
+            sample_day: int,
+            sample_var_names: list[str],
+            sample_var_vals: list[float]
+            ):
+        """Replace the specified day with new data
+            Created by Yu Luo (yu.8.luo@gsk.com)
+            Created: 2023-10-09
+            Modified: 2023-10-09
+        """
+        self.data.loc[self.data['Day'] == sample_day,sample_var_names] = sample_var_vals
+
+    def predict(self):
+        """Predict the next-day state"""
 
 
 class Controller:
@@ -29,30 +56,38 @@ class Controller:
 
     def __init__(
             self,
-            model,
+            controller_model,
             bioreactor: Bioreactor,
             curr_time: int, # Current culture day
-            setpoint: np.array, # A T by 2 array (time and value)
-            proc_var: list[str], # Controlled process variable
-            proc_var_wt: np.array,
-            man_vars: list[str], # Manipulated variables
-            man_vars_wt: np.array,
+            pv_sps: np.array, # A T by 2 array (time and value)
+            pv_names: list[str], # Controlled process variable
+            pv_wts: np.array, # SP tracking weights
+            mv_names: list[str], # Manipulated variables
+            mv_wts: np.array, # MV cost weights
             pred_horizon: int,
             ctrl_horizon: int,
-            constr: np.array, # A U by 2 array (lower and upper limits)
+            constr: np.array, # A U by 2 array (lower and upper limits only)
     ):
-        self.model = model
+        self.controller_model = controller_model
         self.bioreactor = bioreactor
         self.curr_time = curr_time
-        self.setpoint = setpoint
-        self.proc_var = proc_var
-        self.proc_var_wt = proc_var_wt
-        self.man_vars = man_vars
-        self.man_vars_wt = man_vars_wt
+        self.pv_sps = pv_sps
+        self.pv_names = pv_names
+        self.pv_wts = pv_wts
+        self.mv_names = mv_names
+        self.mv_wts = mv_wts
         self.pred_horizon = pred_horizon
         self.ctrl_horizon = ctrl_horizon
         self.constr = constr
 
+    def est_state(self):
+        """Estimate the current state based on previous measurement"""
+
+
+    def optimize(self):
+        """Optimize future inputs"""
+
+        
     def obj_func(
             self,
             ts: np.array,
@@ -63,17 +98,17 @@ class Controller:
         # Trim to keep only future entries
         x2 = x[ts[:,0] > self.curr_time,:]
         u2 = u[ts[:,0] >= self.curr_time,:]
-        setpoint2 = self.setpoint[self.setpoint[:,0] > self.curr_time,1:]
+        pv_sps2 = self.pv_sps[self.pv_sps[:,0] > self.curr_time,1:]
 
         # Trim to keep the prediction and control horizons
         x3 = x2[0:self.pred_horizon,:]
-        setpoint3 = setpoint2[0:self.pred_horizon,1]
+        pv_sps3 = pv_sps2[0:self.pred_horizon,1]
         u3 = u2[0:self.ctrl_horizon,:]
 
         # Calculate the cost
         u3_diff = np.diff(u3, axis = 0)
-        u3_cost = np.sum(np.multiply(np.sum(np.square(u3_diff),axis = 0),self.man_vars_wt))
-        x3_diff = x3 - setpoint3
-        x3_cost = np.sum(np.multiply(np.sum(np.square(x3_diff),axis = 0),self.proc_var_wt))
+        u3_cost = np.sum(np.multiply(np.sum(np.square(u3_diff),axis = 0),self.mv_wts))
+        x3_diff = x3 - pv_sps3
+        x3_cost = np.sum(np.multiply(np.sum(np.square(x3_diff),axis = 0),self.pv_wts))
         return u3_cost + x3_cost
 
