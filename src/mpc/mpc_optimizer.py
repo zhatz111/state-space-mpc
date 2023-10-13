ab5505ceb2aa0043fc9727c565b7b30ecafd6e3f
Yu Luo <yu.8.luo@gsk.com>
2023-10-13 12:37:36 -0400

    mpc_optimizer.py: wip with controller methods

--- src/mpc/mpc_optimizer.py
+++ src/mpc/mpc_optimizer.py
@@ -69,19 +69,21 @@ class Controller:
             self,
             controller_model,
             bioreactor: Bioreactor,
-            curr_time: int, # Current culture day
-            pv_sps: np.array, # A T by 2 array (time and value)
-            pv_names: list[str], # Controlled process variable
+            ts: np.array, # A 1D, length-T array of time
+            pv_sps: np.array, # A T by P array (P process variables)
+            pv_names: list[str], # Controlled process variable names
             pv_wts: np.array, # SP tracking weights
             mv_names: list[str], # Manipulated variables
             mv_wts: np.array, # MV cost weights
             pred_horizon: int,
             ctrl_horizon: int,
-            constr: np.array, # A U by 2 array (lower and upper limits only)
+            constr: np.array, # A 2 by U array (lower and upper limits only)
+            curr_time = 0 # Current culture day
     ):
         self.controller_model = controller_model
         self.bioreactor = bioreactor
         self.curr_time = curr_time
+        self.ts = ts,
         self.pv_sps = pv_sps
         self.pv_names = pv_names
         self.pv_wts = pv_wts
@@ -93,12 +95,58 @@ class Controller:
 
     def est_state(self):
         """Estimate the current state based on previous measurement"""
+        # Gather previous day's state and input
+        data_prev = self.data.loc[
+            self.data[:,'Day'] == self.curr_time,:]
+        x_prev = data_prev[0,self.controller_model.states]
+        u_prev = data.prev[0,self.controller_model.states]
+        x_curr = self.controller_model.predict(x_prev,u_prev)
+
+        # Replace current missing data with estimated
 
 
     def optimize(self):
         """Optimize future inputs"""
 
+        # Retrieve MVs from curr_time to EoR
+        data = self.bioreactor.data
+        mv_matrix = data.loc[data['Day'] >= self.curr_time,self.mv_names].values
+
+        # Flatten initial mv
+        mv_array = mv_matrix.flatten()
         
+        # Create constraint matrix
+        constr_low_matrix = np.tile(self.constr[0,:],(mv_matrix.shape[0],1))
+        constr_low_array = constr_low_matrix.flatten()
+        constr_high_matrix = np.tile(self.constr[1,:],(mv_matrix.shape[0],1))
+        constr_high_array = constr_high_matrix.flatten()
+
+        # Solve the optimization problem
+
+
+    def obj_func_wrapper(self,mv_array):
+        """Objective function wrapper"""
+        
+        # Fold mv_array to a 2D array
+        mv_matrix = mv_array.reshape([-1,len(self.mv_names)])
+
+        # Retrieve input from curr_time to EoR
+        u_matrix = self.bioreactor.data.loc[
+            self.bioreactor.data['Day'] >= self.curr_time,
+            self.controller_model.inputs].values
+
+        # Replace MVs with mv_matrix
+        loc_mv_in_inputs = np.where(np.isin(self.controller_model.inputs,self.mv_names))[0]
+        u_matrix[:,loc_mv_in_inputs] = mv_matrix
+        print(u_matrix)
+
+        # # Sim
+        # xHat = self.controller_model.predict()
+
+        # # Obj
+        # return self.obj_func(ts,xHat,u)
+        
+
     def obj_func(
             self,
             ts: np.array,
