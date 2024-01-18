971e81f901f4dd045fb88ef111514436df81c720
Yu Luo <yu.8.luo@gsk.com>
2024-01-18 15:08:15 -0500

    estimation.py: specify CURR_TIME and VESSEL in the beginning of the code; use the master sheet's file name as output folder's name; only use the correct vessel/bioreactor in the reference data; same model for bioreactor and controller; added verification checks to compare weights and pvs/mvs lengths; user can specify and override the mod values; follow the estimate, next_day, optimize, return_data order

--- src/estimation.py
+++ src/estimation.py
@@ -1,7 +1,7 @@
 """Main code for simulating closed-loop MPC
-    Created by Yu Luo (yu.8.luo@gsk.com)
+    Created by Yu Luo (yu.8.luo@gsk.com) and Zach Hatzenbeller (zach.a.hatzenbeller@gsk.com)
     Created: 2023-10-05
-    Modified: 2023-10-05
+    Modified: 2024-01-18
 """
 
 # pylint: disable=locally-disabled, multiple-statements, fixme, no-name-in-module
@@ -26,75 +26,64 @@ warnings.filterwarnings("ignore")
 
 todays_date = datetime.today().strftime('%Y-%m-%d')
 
-# Load an example dataset
+# Specify the current time and vessel
+CURR_TIME = 2
+VESSEL = 1
+
+# Load an example read-only "master" sheet
 SIM_FOLDER = "mpc-simulation"
-SIM_REFERENCE_DATA = "control_data_template"
+SIM_REFERENCE_DATA = f"master_sheet_example-D{CURR_TIME}"
 
 simulation_path = Path(
     "~/GSK/Biopharm Model Predictive Control - General/data/", SIM_FOLDER
 )
-reference_data = pd.read_csv(Path(simulation_path, f"{SIM_REFERENCE_DATA}.csv"))
+reference_data_all = pd.read_csv(Path(simulation_path, f"{SIM_REFERENCE_DATA}.csv"))
 
 # Create output folder
 batch_sheet_path = Path(simulation_path.expanduser(), SIM_REFERENCE_DATA)
 batch_sheet_path.mkdir(parents=True, exist_ok=True)
 
 # Load the model
-CONTROL_MATRIX_FOLDER_EXT = "Control_Model_Matrices"
+# CONTROL_MATRIX_FOLDER_EXT = "Control_Model_Matrices"
 SIM_MATRIX_FOLDER_EXT = "Sim_Model_Matrices"
 
 # Parse the states from the reference data csv file
-contains_state_data = reference_data.columns.str.contains("--STATE_DATA")
-contains_input = reference_data.columns.str.contains("--INPUT_DATA")
+reference_data_this_vessel = reference_data_all.loc[reference_data_all["Bioreactor"] == VESSEL,]
+contains_state_data = reference_data_this_vessel.columns.str.contains("--STATE_DATA")
+contains_input = reference_data_this_vessel.columns.str.contains("--INPUT_DATA")
 
 # store the states and inputs as a list
-STATES = [x.split("--")[0] for x in reference_data.columns[contains_state_data]]
-INPUTS = [x.split("--")[0] for x in reference_data.columns[contains_input]]
+STATES = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_state_data]]
+INPUTS = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_input]]
 
-# STATES = [
-#     "IGG",
-#     "VCC",
-#     "Viability",
-#     "Lactate",
-#     "Osmo",
-#     "pCO2_at_Temp",
-# ]
-
-# INPUTS = [
-#     "Cumulative_Normalized_Feed",
-#     "Temperature",
-#     "pH_setpoint",
-#     "DO",
-# ]
-
-# Controller Matrices: Import matrices and scaler (worse model)
-controller_model_scaler = joblib.load(
-    Path(
-        simulation_path.expanduser(),
-        rf"{CONTROL_MATRIX_FOLDER_EXT}",
-        "model_scaler.scl",
-    ),
-)
-control_A_matrix = np.array(
-    pd.read_csv(
-        Path(
-            simulation_path.expanduser(),
-            rf"{CONTROL_MATRIX_FOLDER_EXT}",
-            "A_Matrix.csv",
-        ),
-        header=None,
-    )
-)
-control_B_matrix = np.array(
-    pd.read_csv(
-        Path(
-            simulation_path.expanduser(),
-            rf"{CONTROL_MATRIX_FOLDER_EXT}",
-            "B_Matrix.csv",
-        ),
-        header=None,
-    )
-)
+# # Controller Matrices: Import matrices and scaler (worse model)
+# controller_model_scaler = joblib.load(
+#     Path(
+#         simulation_path.expanduser(),
+#         rf"{CONTROL_MATRIX_FOLDER_EXT}",
+#         "model_scaler.scl",
+#     ),
+# )
+# control_A_matrix = np.array(
+#     pd.read_csv(
+#         Path(
+#             simulation_path.expanduser(),
+#             rf"{CONTROL_MATRIX_FOLDER_EXT}",
+#             "A_Matrix.csv",
+#         ),
+#         header=None,
+#     )
+# )
+# control_B_matrix = np.array(
+#     pd.read_csv(
+#         Path(
+#             simulation_path.expanduser(),
+#             rf"{CONTROL_MATRIX_FOLDER_EXT}",
+#             "B_Matrix.csv",
+#         ),
+#         header=None,
+#     )
+# )
 
 # Simulation Matrices: Import matrices and scaler (better model)
 sim_model_scaler = joblib.load(
@@ -115,13 +104,13 @@ sim_B_matrix = np.array(
     )
 )
 
-# Create a state space model for control
+# Create a state space model for control (identical to bioreactor sim model)
 controller_model = StateSpaceModel(
     states=STATES,
     inputs=INPUTS,
-    scaler=controller_model_scaler,
-    a_matrix=control_A_matrix,
-    b_matrix=control_B_matrix,
+    scaler=sim_model_scaler,
+    a_matrix=sim_A_matrix,
+    b_matrix=sim_B_matrix,
 )
 
 # Create a state space model for simulation
@@ -135,22 +124,22 @@ simulation_model = StateSpaceModel(
 
 # Construct a bioreactor object
 bioreactor = Bioreactor(
-    vessel="BR1-MPC", process_model=simulation_model, data=reference_data
+    vessel=VESSEL, process_model=simulation_model, data=reference_data_this_vessel
 )
 
 # Parse the PV and MV names from the reference data csv file
-contains_PV = reference_data.columns.str.contains("state_sp", case=False)
-contains_MV = reference_data.columns.str.contains("input_ref", case=False)
+contains_PV = reference_data_this_vessel.columns.str.contains("state_sp", case=False)
+contains_MV = reference_data_this_vessel.columns.str.contains("input_ref", case=False)
 
 # Construct a controller object
 PRED_HORIZON = 30
 CTRL_HORIZON = 3
-CURR_TIME = 0
-ts = np.array(reference_data["Day"])
+EST_HORIZON = 2
+ts = np.array(reference_data_this_vessel["Day"])
 
 # Define the PV and MV names using the parsing from csv file
-PV_NAMES = [x.split("--")[0] for x in reference_data.columns[contains_PV]]
-MV_NAMES = [x.split("--")[0] for x in reference_data.columns[contains_MV]]
+pv_names = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_PV]]
+mv_names = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_MV]]
 
 PV_WTS = np.array([1 / (1000) ** 2])
 EST_WTS = np.array([2.59074E-07, 0.078638189, 0.033750463, 2.614604943, 0.006617413, 0.018507072])
@@ -161,104 +150,50 @@ MV_BOUNDS = np.array([[0, 0.1]])  # feed
 # Define the suffix after each MV name and index the matrix with these names
 PV_SUFFIX = "--STATE_SP"
 MV_SUFFIX = "--INPUT_REF"
-pv_sps = reference_data[[mv + PV_SUFFIX for mv in PV_NAMES]].values
-mv_matrix = reference_data[[mv + MV_SUFFIX for mv in MV_NAMES]].values
+pv_sps = reference_data_this_vessel[[mv + PV_SUFFIX for mv in pv_names]].values
+mv_matrix = reference_data_this_vessel[[mv + MV_SUFFIX for mv in mv_names]].values
+
+# Verify dimensions (YL@2024-01-18)
+if len(EST_WTS) != len(STATES):
+    raise ValueError("Wrong estimation weights dimension!")
+if len(PV_WTS) != len(pv_names):
+    raise ValueError("Wrong PV weights dimension!")
+if len(MV_WTS) != len(mv_names):
+    raise ValueError("Wrong PV weights dimension!")
 
 controller = Controller(
     controller_model=controller_model,
     bioreactor=bioreactor,
     ts=ts,
     pv_sps=pv_sps,
-    pv_names=PV_NAMES,
+    pv_names=pv_names,
     pv_wts=PV_WTS,
-    mv_names=MV_NAMES,
+    mv_names=mv_names,
     mv_wts=MV_WTS,
     pred_horizon=PRED_HORIZON,
     ctrl_horizon=CTRL_HORIZON,
     constr=MV_BOUNDS,
-    delta_p=np.ones((1, len(STATES))),
+    output_mods_user=np.array([]),
     est_wts=EST_WTS,
+    est_horizon=EST_HORIZON,
 )
 
-# Simulate a DoE to Determine what factors to test in-silico
+# Update the time cursor
+bioreactor.curr_time = CURR_TIME
 
-# Day 3-4, 6-7, and 9-10 pH and temp setpoints
-DOE_FACTOR_LEVELS = [
-    [18, 7.05, 33],
-    [15, 7.35, 35],
-    [15, 7.05, 33],
-    [18, 7.20, 34],
-    [12, 7.20, 34],
-    [12, 7.35, 35],
-    [12, 7.05, 33],
-    [15, 7.35, 35],
-    # [18, 7.35, 35],
-]
+# Estimate CURR_DAY's state
+controller.estimate() # Update bioreactor.data>STATE_MOD (curr day)
+bioreactor.next_day() # Update bioreactor.data>STATE_EST (curr day + 1)
+controller.optimize(open_loop=False) # Update bioreactor.data>STATE_PRED (curr day + 1:end of pred horizon)
 
-# Create a list of bioreactors for the DoE
-NUM_REACTORS = len(DOE_FACTOR_LEVELS)
+bioreactor.return_data(show_daily_feed=True).to_csv(
+    batch_sheet_path / f"{bioreactor.vessel}_daily_feed_D{CURR_TIME}-{todays_date}.csv"
+)
+bioreactor.return_data(show_daily_feed=False).to_csv(
+    batch_sheet_path / f"{bioreactor.vessel}_total_feed_D{CURR_TIME}-{todays_date}.csv"
+)
 
-sim_bioreactors = [
-    Bioreactor(
-        vessel=f"BR{i}",
-        process_model=simulation_model,
-        data=reference_data,
-    )
-    for i in range(1, NUM_REACTORS + 1)
-]
-controllers = [
-    Controller(
-        controller_model=simulation_model,
-        bioreactor=sim_bioreactors[count],
-        ts=ts,
-        pv_sps=pv_sps,
-        pv_names=PV_NAMES,
-        pv_wts=PV_WTS,
-        mv_names=MV_NAMES,
-        mv_wts=MV_WTS,
-        pred_horizon=PRED_HORIZON,
-        ctrl_horizon=CTRL_HORIZON,
-        constr=MV_BOUNDS,
-        delta_p=controller.delta_p,
-        est_wts=EST_WTS,
-    )
-    for count, _ in enumerate(sim_bioreactors)
-]
-
-# Change bioreactor data based on DoE setpoints
-for count, bioreactor in enumerate(sim_bioreactors):
-    # Change day 0's iVCC setpoints
-    bioreactor.data["VCC--STATE_DATA"].iloc[0] = DOE_FACTOR_LEVELS[count][0]
-
-    # Change day 3's pH and Temp setpoints
-    bioreactor.data["PH_SETPOINT--INPUT_DATA"].iloc[3] = DOE_FACTOR_LEVELS[count][1]
-    bioreactor.data["TEMPERATURE--INPUT_DATA"].iloc[3] = DOE_FACTOR_LEVELS[count][2]
-
-    # Change day 9's pH and Temp setpoints
-    bioreactor.data["PH_SETPOINT--INPUT_DATA"].iloc[9] = DOE_FACTOR_LEVELS[count][1]
-    bioreactor.data["TEMPERATURE--INPUT_DATA"].iloc[9] = DOE_FACTOR_LEVELS[count][2]
-
-    # Open loop simulation
-    _, bioreactor.open_loop_df = bioreactor.sim_from_curr_day()
-
-# Simulate all the bioreactors and each controller and get a dictionary output
-DOE_dict = {}
-CURR_DAY = 3
-
-for bioreactor, controller in zip(sim_bioreactors, controllers):
-    controller.curr_time = CURR_DAY
-    bioreactor.curr_time = CURR_DAY
-    controller.estimator(3)
-    controller.curr_time = CURR_DAY + 1
-    bioreactor.curr_time = CURR_DAY + 1
-    controller.optimize(open_loop=False)
-    # bioreactor.next_day()
-    DOE_dict[bioreactor.vessel] = bioreactor.return_data()
-    bioreactor.return_data().to_csv(
-        simulation_path / "daily_control_files" / f"{todays_date}_{bioreactor.vessel}.csv"
-    )
-
-# Plot the in-silico Simulations
-br_plots = MPCVisualizer(sim_bioreactors, controllers)
-br_plots.plot_simulations()
-# br_plots.output_table().to_clipboard()
+# # Plot the in-silico Simulations
+# br_plots = MPCVisualizer(sim_bioreactors, controllers)
+# br_plots.plot_simulations()
+# # br_plots.output_table().to_clipboard()
