56d0228fed2e77a699e2b4d1fa5a43764d852203
Zach Hatzenbeller <zach.a.hatzenbeller@gsk.com>
2024-02-07 16:01:10 -0500

    reorganized code, better documentation

--- src/estimation.py
+++ src/estimation.py
@@ -8,101 +8,104 @@
 # pylint: disable=locally-disabled, multiple-statements, fixme, import-error
 
 # Standard Library Imports
+import sys
 import warnings
 from pathlib import Path
 from datetime import datetime
 
 # 3rd Party Library Imports
-import joblib
 import numpy as np
 import pandas as pd
 
 # State-Space-Model Package Imports
 from mpc.mpc_optimizer import Bioreactor, Controller
 from visualization.visualize import MPCVisualizer
-from models.ssm import StateSpaceModel
+from models.ssm import StateSpaceModel, json_toscaler
 
 warnings.filterwarnings("ignore")
 
-todays_date = datetime.today().strftime('%Y-%m-%d')
+# Store todays date and the top level directory in variables
+todays_date = datetime.today().strftime("%Y-%m-%d")
+top_dir = Path().absolute()
 
-# Specify the current time and vessel
+# -------------------------------------------------------------------------------------
+# USER SPECIFIED DATA
+
+# Specify the study number, current time and vessel
+EXP_NUM = "AR24-005"
 CURR_TIME = 2
-VESSEL = 1
+VESSEL = 1  # want to eliminate this and add a for loop for all reactors
 
-# Load an example read-only "master" sheet
+# Specify names for batch sheet parent folder and master sheet
 SIM_FOLDER = "mpc-simulation"
 SIM_REFERENCE_DATA = f"master_sheet_example-D{CURR_TIME}"
 
+# Specify batch sheet path and load the read-only "master" sheet
 simulation_path = Path(
     "~/GSK/Biopharm Model Predictive Control - General/data/", SIM_FOLDER
 )
-reference_data_all = pd.read_csv(Path(simulation_path, f"{SIM_REFERENCE_DATA}.csv"))
-
-# Create output folder
-batch_sheet_path = Path(simulation_path.expanduser(), SIM_REFERENCE_DATA)
-batch_sheet_path.mkdir(parents=True, exist_ok=True)
-
-# Load the model
-# CONTROL_MATRIX_FOLDER_EXT = "Control_Model_Matrices"
-SIM_MATRIX_FOLDER_EXT = "Sim_Model_Matrices"
-
-# Parse the states from the reference data csv file
-reference_data_this_vessel = reference_data_all.loc[reference_data_all["Bioreactor"] == VESSEL, :]
-contains_state_data = reference_data_this_vessel.columns.str.contains("--STATE_DATA")
-contains_input = reference_data_this_vessel.columns.str.contains("--INPUT_DATA")
-
-# store the states and inputs as a list
-STATES = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_state_data]]
-INPUTS = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_input]]
-
-# # Controller Matrices: Import matrices and scaler (worse model)
-# controller_model_scaler = joblib.load(
-#     Path(
-#         simulation_path.expanduser(),
-#         rf"{CONTROL_MATRIX_FOLDER_EXT}",
-#         "model_scaler.scl",
-#     ),
-# )
-# control_A_matrix = np.array(
-#     pd.read_csv(
-#         Path(
-#             simulation_path.expanduser(),
-#             rf"{CONTROL_MATRIX_FOLDER_EXT}",
-#             "A_Matrix.csv",
-#         ),
-#         header=None,
-#     )
-# )
-# control_B_matrix = np.array(
-#     pd.read_csv(
-#         Path(
-#             simulation_path.expanduser(),
-#             rf"{CONTROL_MATRIX_FOLDER_EXT}",
-#             "B_Matrix.csv",
-#         ),
-#         header=None,
-#     )
-# )
-
-# Simulation Matrices: Import matrices and scaler (better model)
-sim_model_scaler = joblib.load(
-    Path(simulation_path.expanduser(), rf"{SIM_MATRIX_FOLDER_EXT}", "model_scaler.scl")
+reference_data_all = pd.read_csv(
+    Path(top_dir, f"data/simulation/{EXP_NUM}", f"{SIM_REFERENCE_DATA}.csv")
 )
 
+# -------------------------------------------------------------------------------------
+# LOAD MODEL DATA
+
+# Import the MinMaxScaler Json file
+json_scaler_path = Path(top_dir, f"data/parameters/{EXP_NUM}", "model_scaler.json")
+sim_model_scaler = json_toscaler(json_file=json_scaler_path)
+
+# Import the A and B matrix CSV files
 sim_A_matrix = np.array(
     pd.read_csv(
-        Path(simulation_path.expanduser(), rf"{SIM_MATRIX_FOLDER_EXT}", "A_Matrix.csv"),
+        Path(top_dir, f"data/parameters/{EXP_NUM}", "A_Matrix.csv"),
         header=None,
     )
 )
 
 sim_B_matrix = np.array(
     pd.read_csv(
-        Path(simulation_path.expanduser(), rf"{SIM_MATRIX_FOLDER_EXT}", "B_Matrix.csv"),
+        Path(top_dir, f"data/parameters/{EXP_NUM}", "B_Matrix.csv"),
         header=None,
     )
 )
+
+# -------------------------------------------------------------------------------------
+# DIRECTORY CREATION AND PARSING OF STATES & INPUTS
+
+# Create output folder
+batch_sheet_path = Path(simulation_path.expanduser(), SIM_REFERENCE_DATA)
+batch_sheet_path.mkdir(parents=True, exist_ok=True)
+
+# Parse the states from the reference data csv file
+reference_data_this_vessel = reference_data_all.loc[
+    reference_data_all["Bioreactor"] == VESSEL, :
+]
+contains_state_data = reference_data_this_vessel.columns.str.contains("--STATE_DATA")
+contains_input = reference_data_this_vessel.columns.str.contains("--INPUT_DATA")
+
+# store the states and inputs as a list
+STATES = [
+    x.split("--")[0] for x in reference_data_this_vessel.columns[contains_state_data]
+]
+INPUTS = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_input]]
+
+# Parse the PV and MV names from the reference data csv file
+PV_SUFFIX = "--STATE_SP"
+MV_SUFFIX = "--INPUT_REF"
+contains_PV = reference_data_this_vessel.columns.str.contains(PV_SUFFIX, case=False)
+contains_MV = reference_data_this_vessel.columns.str.contains(MV_SUFFIX, case=False)
+
+# Define the PV and MV names using the parsing from csv file
+pv_names = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_PV]]
+mv_names = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_MV]]
+
+# Define the suffix after each MV name and index the matrix with these names
+pv_sps = reference_data_this_vessel[[mv + PV_SUFFIX for mv in pv_names]].values
+mv_matrix = reference_data_this_vessel[[mv + MV_SUFFIX for mv in mv_names]].values
+
+# -------------------------------------------------------------------------------------
+# MODEL, BIOREACTOR, & CONTROLLER CLASS INSTANTIATION
 
 # Create a state space model for control (identical to bioreactor sim model)
 controller_model = StateSpaceModel(
@@ -113,52 +116,29 @@ controller_model = StateSpaceModel(
     b_matrix=sim_B_matrix,
 )
 
-# Create a state space model for simulation
-simulation_model = StateSpaceModel(
-    states=STATES,
-    inputs=INPUTS,
-    scaler=sim_model_scaler,
-    a_matrix=sim_A_matrix,
-    b_matrix=sim_B_matrix,
-)
-
 # Construct a bioreactor object
 bioreactor = Bioreactor(
-    vessel=VESSEL, process_model=simulation_model, data=reference_data_this_vessel
+    vessel=VESSEL, process_model=controller_model, data=reference_data_this_vessel
 )
-
-# Parse the PV and MV names from the reference data csv file
-contains_PV = reference_data_this_vessel.columns.str.contains("state_sp", case=False)
-contains_MV = reference_data_this_vessel.columns.str.contains("input_ref", case=False)
 
 # Construct a controller object
 PRED_HORIZON = 30
 CTRL_HORIZON = 3
 EST_HORIZON = 2
 ts = np.array(reference_data_this_vessel["Day"])
-
-# Define the PV and MV names using the parsing from csv file
-pv_names = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_PV]]
-mv_names = [x.split("--")[0] for x in reference_data_this_vessel.columns[contains_MV]]
-
 PV_WTS = np.array([1 / (1000) ** 2])
-EST_WTS = np.array([
-    2.5E-07,    # IGG
-    0.08,       # VCC
-    0.03,       # Viability
-    0.05,       # Lactate
-    0.007,      # OSMO
-    0.001       # CO2
-    ])
-
 MV_WTS = np.array([1 / (0.01) ** 2])
 MV_BOUNDS = np.array([[0, 0.1]])  # feed
-
-# Define the suffix after each MV name and index the matrix with these names
-PV_SUFFIX = "--STATE_SP"
-MV_SUFFIX = "--INPUT_REF"
-pv_sps = reference_data_this_vessel[[mv + PV_SUFFIX for mv in pv_names]].values
-mv_matrix = reference_data_this_vessel[[mv + MV_SUFFIX for mv in mv_names]].values
+EST_WTS = np.array(
+    [
+        2.5e-07,  # IGG
+        0.08,  # VCC
+        0.03,  # Viability
+        0.05,  # Lactate
+        0.007,  # OSMO
+        0.001,  # CO2
+    ]
+)
 
 # Verify dimensions (YL@2024-01-18)
 if len(EST_WTS) != len(STATES):
@@ -188,23 +168,47 @@ controller = Controller(
     est_horizon=EST_HORIZON,
 )
 
+# -------------------------------------------------------------------------------------
+# MAIN MPC LOOP ESTIMATES & OPTIMIZES EACH BIOREACTOR
+
 # Update the time cursor
 bioreactor.curr_time = CURR_TIME
 
 # Estimate CURR_DAY's state
-controller.estimate() # Update bioreactor.data>STATE_MOD (curr day)
-bioreactor.next_day() # Update bioreactor.data>STATE_EST (curr day + 1)
-controller.optimize(open_loop=False) # Update bioreactor.data>STATE_PRED (curr day + 1:end of pred horizon)
+
+# Update bioreactor.data>STATE_MOD (curr day)
+controller.estimate()
+# Update bioreactor.data>STATE_EST (curr day + 1)
+bioreactor.next_day()
+# Update bioreactor.data>STATE_PRED (curr day + 1:end of pred horizon)
+controller.optimize(open_loop=False)
+
+# -------------------------------------------------------------------------------------
+# BIOREACTOR DATA SAVED
 
 bioreactor.return_data(show_daily_feed=True).to_csv(
-    batch_sheet_path / f"BR{bioreactor.vessel:02d}_daily_feed_D{CURR_TIME}-{todays_date}.csv"
+    batch_sheet_path
+    / f"BR{bioreactor.vessel:02d}_daily_feed_D{CURR_TIME}-{todays_date}.csv"
 )
 bioreactor.return_data(show_daily_feed=False).to_csv(
-    batch_sheet_path / f"BR{bioreactor.vessel:02d}_total_feed_D{CURR_TIME}-{todays_date}.csv"
+    batch_sheet_path
+    / f"BR{bioreactor.vessel:02d}_total_feed_D{CURR_TIME}-{todays_date}.csv"
 )
 
-# # Plot the in-silico Simulations
+# -------------------------------------------------------------------------------------
+# GENERATED PLOTS SAVED
+
+# Plot the MPC Controller for each Bioreactor
 br_plots = MPCVisualizer(bioreactor, controller)
-br_plots.mpc_daily_plot()
-# br_plots.plot_simulations()
-# # br_plots.output_table().to_clipboard()
+br_plots.mpc_daily_plot(
+    save_path=batch_sheet_path
+    / f"BR{bioreactor.vessel:02d}_D{CURR_TIME}-{todays_date}.png",
+    metadata={
+        "Title": f"{EXP_NUM}-BR{bioreactor.vessel:02d}-D{CURR_TIME}",
+        "Author": "Zach Hatzenbeller, Yu Luo",
+        "Description": f"MPC plot for {EXP_NUM}. Developed within GSK R&D in BDSD",
+        "Copyright": f"(c) GSK, R&D, BDSD {datetime.today().year}",
+        "Creation Time": f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+        "Software": f"Python v{sys.version}",
+    },
+)
