6aed1ef2e6339c3c0c731d0c1fa46463d863ea7d
Zach Hatzenbeller <zach.a.hatzenbeller@gsk.com>
2024-02-07 16:00:19 -0500

    added functionality to store scaler as json

--- src/models/ssm.py
+++ src/models/ssm.py
@@ -2,14 +2,76 @@
 """
 # Standard library imports
 from typing import Union
+from pathlib import Path
 
 # Third party library imports
+import json
 import numpy as np
 from scipy.signal import lsim, StateSpace
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 # Create type hint for the scaler object being passed to SSM class
 ScalerType = Union[MinMaxScaler, StandardScaler]
+
+
+def scaler_tojson(scaler: MinMaxScaler, save_path: Union[str, Path]):
+    """
+    The function `scaler_tojson` takes a scaler object and saves its attributes to a JSON file.
+    
+    Args:
+      scaler: The `scaler` parameter is an instance of a scaler object. It could be any scaler object
+    from a machine learning library, such as `StandardScaler` from scikit-learn. The scaler object is
+    used to scale or normalize data.
+      save_path: The `save_path` parameter is the file path where the JSON file will be saved. It should
+    include the file name and extension. For example, if you want to save the JSON file as
+    "scaler_attributes.json" in the current directory, you can set `save_path` as "sc
+    """
+    # Prepare a dictionary to hold the scaler's attributes
+    scaler_attributes = {
+        attr_name: getattr(scaler, attr_name).tolist()
+        if hasattr(getattr(scaler, attr_name), "tolist")
+        else getattr(scaler, attr_name)
+        for attr_name in vars(scaler)
+    }
+
+    # Save the attributes to a JSON file
+    with open(save_path, "w", encoding="utf-8") as file:
+        json.dump(scaler_attributes, file, indent=4)
+
+
+def json_toscaler(json_file: Union[str, Path], minmaxscaler=True):
+    """
+    The function `json_toscaler` takes a JSON file containing attributes of a scaler and reconstructs
+    the scaler object using the loaded attributes.
+    
+    Args:
+      json_file: The path to the JSON file that contains the attributes of the scaler object.
+      minmaxscaler: The `minmaxscaler` parameter is a boolean flag that determines whether to use the
+    `MinMaxScaler` class for scaling the data. If `minmaxscaler` is set to `True`, the function will use
+    `MinMaxScaler` for scaling the data. If it is set to `. Defaults to True
+    
+    Returns:
+      a reconstructed scaler object.
+    """
+    # Load the attributes from the JSON file
+    with open(json_file, "r", encoding="utf-8") as file:
+        loaded_attributes = json.load(file)
+
+    # Initialize a new MinMaxScaler instance
+    if minmaxscaler:
+        reconstructed_scaler = MinMaxScaler()
+    else:
+        raise ValueError("This method currently only works for the MinMaxScaler")
+
+    # Set the loaded attributes back to the scaler
+    for attr_name, attr_value in loaded_attributes.items():
+        setattr(
+            reconstructed_scaler,
+            attr_name,
+            np.array(attr_value) if isinstance(attr_value, list) else attr_value,
+        )
+
+    return reconstructed_scaler
 
 
 class StateSpaceModel:
@@ -27,7 +89,7 @@ class StateSpaceModel:
         scaler: ScalerType,
         a_matrix: np.ndarray,
         b_matrix: np.ndarray,
-        name = ''
+        name="",
     ):
         """
         The function initializes an object with given states, inputs, scaler, a_matrix, and b_matrix
@@ -73,8 +135,11 @@ class StateSpaceModel:
         self.input_data_labels = [x + input_suffix for x in self.inputs]
 
     def ssm_lsim(
-        self, initial_state: np.ndarray, input_matrix: np.ndarray, time: np.ndarray,
-        output_mods = np.array([])
+        self,
+        initial_state: np.ndarray,
+        input_matrix: np.ndarray,
+        time: np.ndarray,
+        output_mods=np.array([]),
     ):
         """
         Created by ZH (zach.a.hatzenbeller@gsk.com)
@@ -98,7 +163,7 @@ class StateSpaceModel:
         Returns:
           the predicted trajectory of the system, represented by the variable xHat.
         """
-        # Warning: Remember that the time array needs to start at 0 with the initial 
+        # Warning: Remember that the time array needs to start at 0 with the initial
         # condition at time 0
 
         # Check if X0 is 1d or 2d and reshape accordingly
@@ -113,7 +178,7 @@ class StateSpaceModel:
 
         # Default vector of ones
         if output_mods.size == 0:
-            output_mods = np.ones((1,len(self.states)))
+            output_mods = np.ones((1, len(self.states)))
 
         # Check if U is 1d or 2d and reshape accordingly
         if input_matrix.ndim == 1:
@@ -161,6 +226,6 @@ class StateSpaceModel:
 
         # Modify measurement based on the correction factor delta_p (diagonal of the C matrix)
         # delta_p_matrix = np.tile(output_mods,(x_hat.shape[0],1))
-        y_hat = np.multiply(x_hat,output_mods)
+        y_hat = np.multiply(x_hat, output_mods)
 
-        return x_hat,y_hat
+        return x_hat, y_hat
