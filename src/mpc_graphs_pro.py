9fd45f8823ea583c1f7ceba6e8b44efcb73cdd5f
Yu Luo <yu.8.luo@gsk.com>
2024-03-05 15:18:33 -0500

    mpc_graphs_pro.py: ad hoc fix of an issue of sp line is joined between D0 and D12

--- src/mpc_graphs_pro.py
+++ src/mpc_graphs_pro.py
@@ -37,12 +37,13 @@ daily_glc[total_glc_diff > 0] = total_glc_diff[total_glc_diff > 0]
 df_data["Daily Glucose"] = daily_glc
 df_data_selected = df_data.loc[:,["Bioreactor","Day","Batch","Controller","iVCC","pH","Temp","IGG","VCC","Viability","Lactate","Glucose","pCO2","Temp/pH","Measured","Total Feed","Total Glucose","Daily Feed","Daily Glucose"]]
 
-DISPLAY_VARIABLE = "Daily Glucose"
+DISPLAY_VARIABLE = "IGG"
 
 # Retrieve setpoint from the master sheet directly
 top_dir = Path().absolute()
 sp_path = Path(top_dir, f"data/simulation/AR24-005/ar24-005-mpc.csv")
 df_sp = pd.read_csv(sp_path).rename(columns={"IGG--STATE_SP":"Setpoint"})
+df_sp.loc[df_sp["Day"] == 0,"Setpoint"] = float("NaN")
 df_sp_selected = df_sp.loc[:,["Bioreactor","Day","Setpoint"]]
 
 # Left join the two dfs
@@ -76,6 +77,7 @@ def data_grp_by_ctrl(data, **kwargs):
     )
     if DISPLAY_VARIABLE == "IGG":
         plt.plot(df_joined["Day"], df_joined["Setpoint"], "b--")
+    
     plt.grid(axis="x", linestyle="--", color="gray")
     plt.grid(axis="y", linestyle="--", color="gray")
 
@@ -85,41 +87,41 @@ g.add_legend(title="iVCC")
 # plt.show()
 plt.savefig(fname=Path(fig_path,f"{DISPLAY_VARIABLE}_grp_by_ctrl.png"))
 
-# Setpoint tracking (iVCC)
-g = sns.FacetGrid(
-    df_joined,
-    col="iVCC",
-    height=4,
-    sharex=False,
-    sharey=True,
-    despine=False,
-)
+# # Setpoint tracking (iVCC)
+# g = sns.FacetGrid(
+#     df_joined,
+#     col="iVCC",
+#     height=4,
+#     sharex=False,
+#     sharey=True,
+#     despine=False,
+# )
 
-def data_grp_by_ivcc(data, **kwargs):
-    sns.lineplot(
-        x="Day",
-        y=DISPLAY_VARIABLE,
-        hue="Controller",
-        marker="o",
-        hue_order=["Julia", "Python", "No MPC"],
-        palette=["C0", "C1", "k"],
-        markersize=8,
-        err_style="bars",
-        err_kws={"capsize": 2, "elinewidth": 2, "capthick": 2},
-        errorbar="ci",
-        data=data,
-        **kwargs,
-    )
-    if DISPLAY_VARIABLE == "IGG":
-        plt.plot(df_joined["Day"], df_joined["Setpoint"], "b--")
-    plt.grid(axis="x", linestyle="--", color="gray")
-    plt.grid(axis="y", linestyle="--", color="gray")
+# def data_grp_by_ivcc(data, **kwargs):
+#     sns.lineplot(
+#         x="Day",
+#         y=DISPLAY_VARIABLE,
+#         hue="Controller",
+#         marker="o",
+#         hue_order=["Julia", "Python", "No MPC"],
+#         palette=["C0", "C1", "k"],
+#         markersize=8,
+#         err_style="bars",
+#         err_kws={"capsize": 2, "elinewidth": 2, "capthick": 2},
+#         errorbar="ci",
+#         data=data,
+#         **kwargs,
+#     )
+#     if DISPLAY_VARIABLE == "IGG":
+#         plt.plot(df_joined["Day"], df_joined["Setpoint"], "b--")
+#     plt.grid(axis="x", linestyle="--", color="gray")
+#     plt.grid(axis="y", linestyle="--", color="gray")
 
 
-g.map_dataframe(data_grp_by_ivcc)
-g.add_legend(title="MPC Controller")
-# plt.show()
-plt.savefig(fname=Path(fig_path,f"{DISPLAY_VARIABLE}_grp_by_ivcc.png"))
+# g.map_dataframe(data_grp_by_ivcc)
+# g.add_legend(title="MPC Controller")
+# # plt.show()
+# plt.savefig(fname=Path(fig_path,f"{DISPLAY_VARIABLE}_grp_by_ivcc.png"))
 
 if DISPLAY_VARIABLE == "IGG":
 
@@ -150,7 +152,7 @@ if DISPLAY_VARIABLE == "IGG":
             data=data,
             **kwargs,
         )
-        plt.axhline(y=0, color="b", linestyle="-.")
+        plt.axhline(y=0, color="b", linestyle="--")
         plt.grid(axis="x", linestyle="--", color="gray")
         plt.grid(axis="y", linestyle="--", color="gray")
 
@@ -159,40 +161,40 @@ if DISPLAY_VARIABLE == "IGG":
     # plt.show()
     plt.savefig(fname=Path(fig_path,"dev_grp_by_ctrl.png"))
 
-    # Setpoint deviation (iVCC)
-    g = sns.FacetGrid(
-        df_joined,
-        col="iVCC",
-        height=4,
-        sharex=False,
-        sharey=True,
-        despine=False,
-        ylim=(-30, 30),
-    )
-    sns.set_style("white")
+    # # Setpoint deviation (iVCC)
+    # g = sns.FacetGrid(
+    #     df_joined,
+    #     col="iVCC",
+    #     height=4,
+    #     sharex=False,
+    #     sharey=True,
+    #     despine=False,
+    #     ylim=(-30, 30),
+    # )
+    # sns.set_style("white")
 
 
-    def dev_grp_by_ivcc(data, **kwargs):
-        sns.lineplot(
-            x="Day",
-            y="% Difference from Setpoint",
-            hue="Controller",
-            marker="o",
-            hue_order=["Julia", "Python", "No MPC"],
-            palette=["C0", "C1", "k"],
-            markersize=10,
-            err_style="bars",
-            err_kws={"capsize": 2, "elinewidth": 2, "capthick": 2},
-            errorbar="ci",
-            data=data,
-            **kwargs,
-        )
-        plt.axhline(y=0, color="b", linestyle="-.")
-        plt.grid(axis="x", linestyle="--", color="gray")
-        plt.grid(axis="y", linestyle="--", color="gray")
+    # def dev_grp_by_ivcc(data, **kwargs):
+    #     sns.lineplot(
+    #         x="Day",
+    #         y="% Difference from Setpoint",
+    #         hue="Controller",
+    #         marker="o",
+    #         hue_order=["Julia", "Python", "No MPC"],
+    #         palette=["C0", "C1", "k"],
+    #         markersize=10,
+    #         err_style="bars",
+    #         err_kws={"capsize": 2, "elinewidth": 2, "capthick": 2},
+    #         errorbar="ci",
+    #         data=data,
+    #         **kwargs,
+    #     )
+    #     plt.axhline(y=0, color="b", linestyle="-.")
+    #     plt.grid(axis="x", linestyle="--", color="gray")
+    #     plt.grid(axis="y", linestyle="--", color="gray")
 
 
-    g.map_dataframe(dev_grp_by_ivcc)
-    g.add_legend(title="iVCC")
-    # plt.show()
-    plt.savefig(fname=Path(fig_path,"dev_grp_by_ivcc.png"))
+    # g.map_dataframe(dev_grp_by_ivcc)
+    # g.add_legend(title="iVCC")
+    # # plt.show()
+    # plt.savefig(fname=Path(fig_path,"dev_grp_by_ivcc.png"))
